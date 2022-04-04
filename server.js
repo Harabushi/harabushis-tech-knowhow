846f2128959bc1aefcdafe61682391bb0652ef24
Colby Harris <harris.colby@gmail.com>
2022-04-04 04:24:47 -0500

    added homepage templates and some partials

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+// requires for express and controllers
 const express = require('express');
 const routes = require('./controllers');
 
+// sequelize requires
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// html/handlebars requires
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+// session data
 const sess = {
   secret: 'this is the big hash secret',
   cookie: {},
@@ -15,15 +24,22 @@ const sess = {
   })
 };
 
+// setup app and PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// app methods
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+// turns on controller connections
 app.use(routes);
 
+// server connection
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
